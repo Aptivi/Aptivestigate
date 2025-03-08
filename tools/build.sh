a7e3771208a948ae1d97f06e2ceb7439fde31a4f
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-03-08 15:15:33 +0300

    int - Clean scripts up from redundant mentions
    ---
    
    Type: int
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- tools/build.sh
+++ tools/build.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-#   Aptivestigate  Copyright (C) 2024-2025  Aptivi
-#  
-#   This file is part of Aptivestigate
-#  
-#   Aptivestigate is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#  
-#   Aptivestigate is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY, without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#  
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 # Repository root
 ROOTDIR=$( cd -- "$( dirname -- "$0" )/.." &> /dev/null && pwd )
 
@@ -29,7 +12,7 @@ checkerror() {
     fi
 }
 
-# This script builds. Use when you have dotnet installed.
+# This script builds.
 releaseconf=$1
 if [ -z $releaseconf ]; then
 	releaseconf=Release
