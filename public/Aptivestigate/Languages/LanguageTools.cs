ea7cac99b38523b7fe11e34e90eecc0b772f7ac0
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-10-25 19:29:04 +0300

    imp - Moved localization to resources
    ---
    
    We've moved all localization info to resources. This makes sure that we no
    longer experience performance costs.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Aptivestigate/Languages/LanguageTools.cs
+++ public/Aptivestigate/Languages/LanguageTools.cs
@@ -17,20 +17,27 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
-using LocaleStation.Tools;
-using Aptivestigate.Localized;
+using System.Collections.Generic;
+using System.Resources;
 
 namespace Aptivestigate.Languages
 {
     internal static class LanguageTools
     {
-        private const string localType = "Aptivestigate";
+        internal static readonly Dictionary<string, ResourceManager> resourceManagers = new()
+        {
+            { "Aptivestigate", new("Aptivestigate.Resources.Languages.Output.Localizations", typeof(LanguageTools).Assembly) }
+        };
 
         internal static string GetLocalized(string id)
         {
-            if (!LanguageCommon.IsCustomActionDefined(localType))
-                LanguageCommon.AddCustomAction(localType, new(() => LocalStrings.Languages, () => LocalStrings.Localizations, LocalStrings.Translate, LocalStrings.CheckCulture, LocalStrings.ListLanguagesCulture, LocalStrings.Exists));
-            return LanguageCommon.Translate(id);
+            foreach (var resourceManager in resourceManagers.Values)
+            {
+                string resourceLocalization = resourceManager.GetString(id);
+                if (!string.IsNullOrEmpty(resourceLocalization))
+                    return resourceLocalization;
+            }
+            return id;
         }
     }
 }
